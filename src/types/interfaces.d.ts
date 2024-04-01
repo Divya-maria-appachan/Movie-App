c62aec7d0bdafb55c757c8e64e56788bd880a201
divya <20105240@mail.wit.iev>
2024-04-01 18:16:58 +0100

    Movie Details  page components.

--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -20,3 +20,28 @@ export interface BaseMovie {
   export interface BaseMovieList { 
     movies: BaseMovie[];
   } 
+
+ 
+  export interface MovieImage {
+    file_path: string;
+    aspect_ratio?: number; //some props are optional...
+    height?: number;
+    iso_639_1?: string;
+    vote_average?: number;
+    vote_count?: number;
+    width?: number;
+  }
+
+  
+  
+  export interface MovieT extends BaseMovie {
+    genres: {
+      id: number;
+      name: string;
+    }[];
+    production_countries:{
+        iso_3166_1: string;
+        name: string;
+
+    } [];
+  }
